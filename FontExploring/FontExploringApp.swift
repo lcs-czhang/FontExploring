21af81cb9f67719d2bbf2a77b3020b64f2d4b84d
Youzhou Zhang <czhang@lcs.on.ca>
2024-02-02 10:03:52 -0500

    Initial Commit

---
+++ FontExploring/FontExploringApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontExploringApp.swift
+//  FontExploring
+//
+//  Created by Yuzhou Zhang on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct FontExploringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
